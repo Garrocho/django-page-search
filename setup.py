494abe9774b67a7494c568d29ca72e77fb233e12
CharlesGarrocho <charles.garrocho@gmail.com>
2013-07-05 09:03:13 -0300

    adicionado: arquivo de setup da aplicação. #1

---
+++ setup.py
@@ -0,0 +1,99 @@
+import os
+import sys
+from distutils.util import convert_path
+from fnmatch import fnmatchcase
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+standard_exclude = ('*.py', '*.pyc', '*$py.class', '*~', '.*', '*.bak')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
+                                './dist', 'EGG-INFO', '*.egg-info')
+
+def find_package_data(
+    where='.', package='',
+    exclude=standard_exclude,
+    exclude_directories=standard_exclude_directories,
+    only_in_packages=True,
+    show_ignored=False):
+
+    out = {}
+    stack = [(convert_path(where), '', package, only_in_packages)]
+    while stack:
+        where, prefix, package, only_in_packages = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "Directory %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                if (os.path.isfile(os.path.join(fn, '__init__.py'))
+                    and not prefix):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                    stack.append((fn, '', new_package, False))
+                else:
+                    stack.append((fn, prefix + name + '/', package, only_in_packages))
+            elif package or not only_in_packages:
+                # is a file
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        if show_ignored:
+                            print >> sys.stderr, (
+                                "File %s ignored by pattern %s"
+                                % (fn, pattern))
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+
+
+LONG_DESCRIPTION = """
+==================
+django-page-search
+==================
+
+This is a Django-powered page search system for basic websites.
+"""
+
+setup(
+    name='django-page-search',
+    version=version,
+    description="Django-powered website navigation maintenance tool",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: Portuguese",
+    ],
+    keywords=['django', 'pages', 'search'],
+    author='Charles Garrocho',
+    author_email='charles.garrocho@gmail.com',
+    url='https://github.com/CharlesGarrocho/django-page-search',
+    license='BSD',
+    packages=find_packages(),
+    package_data=find_package_data("page-search", only_in_packages=False),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
