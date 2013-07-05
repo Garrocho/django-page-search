8a29d7c178bf934c37dbc26e34ea66eac3e561f4
CharlesGarrocho <charles.garrocho@gmail.com>
2013-07-05 10:33:32 -0300

    removido: replace de conteúdo do nome do site. close #2

--- page_search/__init__.py
+++ page_search/__init__.py
@@ -25,11 +25,10 @@ def search(request):
                 for arquivo in os.listdir(diretorio):
                     with open(diretorio + '/' + arquivo) as arquivo_busca:
                         conteudo_arquivo_busca = arquivo_busca.read()
-                        conteudo_arquivo_busca = substitui_constantes_texto(conteudo_arquivo_busca)
                         regex_busca = re.compile(regex_busca_str.format(busca), re.IGNORECASE)
                         resultados_busca = re.search(regex_busca, conteudo_arquivo_busca)
                         if resultados_busca is not None and 'template' not in diretorio:
-                            pesquisa.append( {'URL': arquivo.split(".html")[0], 'TEXTO': resultados_busca.groups()[0] })
+                            pesquisa.append( {'URL': arquivo.split(".html")[0], 'text': resultados_busca.groups()[0] })
                             conteudo = 'OK'
     rq = RequestContext(request, {
         'pesquisa': pesquisa,
@@ -40,13 +39,3 @@ def search(request):
     })
     rq['SITE_NOME'] = settings.SITE_NOME
     return rq
-
-
-def substitui_constantes_texto(texto):
-    if '{{ SITE_NOME }}' in texto:
-        texto = texto.replace('{{ SITE_NOME }}', settings.SITE_NOME)
-    if '[at]' in texto:
-        texto = texto.replace('[at]', '@')
-        texto = texto.replace('[dot]', '.')
-    #texto = strip_tags(string_value)
-    return texto
